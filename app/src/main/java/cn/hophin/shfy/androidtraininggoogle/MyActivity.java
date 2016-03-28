271a81259586717dfe16d217a2c4b26545015056
shfy <229651638@qq.com>
2016-03-28 16:46:51 +0800

    start another activity

--- app/src/main/java/cn/hophin/shfy/androidtraininggoogle/MyActivity.java
+++ app/src/main/java/cn/hophin/shfy/androidtraininggoogle/MyActivity.java
@@ -1,5 +1,6 @@
 package cn.hophin.shfy.androidtraininggoogle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "cn.hophin.shfy.androidtraininggoogle.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +52,16 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when hte user clicks the Send button
+     */
+    public void sendMessage(View view){
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        EditText editText= (EditText) findViewById(R.id.edit_message);
+        String message=editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
